0e8229021272bd8f4fe77a06aae83491eb7c3a77
Plusone7 <y02121148s@gmail.com>
2017-08-28 19:22:57 +0800

    add show user list page

--- webview/login.js
+++ webview/login.js
@@ -6,26 +6,60 @@ var views = require('co-views');
 var router = new Router();
 const logger = require('koa-logger'); //show post||get log 
 var LocalStrategy = require( 'passport-local' ).Strategy;
-// mongodb
-// var mongodb = require('mongodb');
-// var mongodbServer = new mongodb.Server('localhost', 27017, {
-//     auto_reconnect: true,
-//     poolSize: 10
-// });
-//var db = new mongodb.Db('dbname', mongodbServer); // use 'dbname' db
+
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var db;
+MongoClient.connect("mongodb://localhost:27017/apple", function (err, pDb) {
+    if (err) {
+        return console.dir(err);
+    }
+    db = pDb;
+});
 var render = views(__dirname, {
     map : {html : 'swig'}
 });
 app.use(bodyparser());
 
 var login;
-var user , pwd ;
+var user,pwd;
+var userId = new Array();
+var userName = new Array();
+var userAccount = new Array();
+var time = new Array();
+var tableNum = new Array();
+
+//SHOW USER DATA FUNC
+var showUserInfo = function showUserInfo(done) {
+    console.log('123');
+    var collection = db.collection('user');
+    collection.find({}).sort( { insertTime: -1 } ).toArray(function (err, data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            userId[i] = data[i]._id.toString(),
+            userName[i] = data[i].userName,
+            userAccount[i] = data[i].account,
+            time[i] = data[i].insertTime
+            tableNum[i] = i;
+        }
+      done();
+     });
+};
+
 
 router.get('/',function * (){
     this.body = yield render("index");
 });
 router.get('/admin',function * (){
-    this.body = yield render("admin");
+    yield showUserInfo;
+    this.body = yield render("admin", {
+        // for html's var
+        "userId": userId, 
+        "userName": userName,  
+        "userAccount":userAccount,
+        "time":time,
+        "num": tableNum,
+    });
 });
 
 router.post('/',function * (){
