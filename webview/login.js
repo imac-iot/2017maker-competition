b54d509316f7e59b73886c2246d2428269d2e8ef
Plusone7 <y02121148s@gmail.com>
2017-08-30 13:43:47 +0800

    add sesseion login check and html using koa-statick url

--- webview/login.js
+++ webview/login.js
@@ -5,7 +5,21 @@ var bodyparser = require('koa-bodyparser');
 var views = require('co-views');
 var router = new Router();
 const logger = require('koa-logger'); //show post||get log 
-var LocalStrategy = require( 'passport-local' ).Strategy;
+const session=require('koa-session');
+const static = require('koa-static')
+const path = require('path')
+const staticPath = './img/'
+app.use(static(
+    path.join( __dirname,staticPath)
+  ))
+app.keys = ['this is my secret']; 
+app.use(session({
+  key: 'koa:sess', // cookie name
+  maxAge: 720000, // (number) maxAge in ms (default is 1 days) 
+  overwrite: true, //(boolean) can overwrite or not (default true) //
+  httpOnly: true, // (boolean) httpOnly or not (default true) //
+  signed: true, // (boolean) signed or not (default true) //
+},app));
 
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
@@ -31,10 +45,9 @@ var tableNum = new Array();
 
 //SHOW USER DATA FUNC
 var showUserInfo = function showUserInfo(done) {
-    console.log('123');
     var collection = db.collection('user');
     collection.find({}).sort( { insertTime: -1 } ).toArray(function (err, data) {
-        console.log(data);
+        //onsole.log(data);
         for (var i = 0; i < data.length; i++) {
             userId[i] = data[i]._id.toString(),
             userName[i] = data[i].userName,
@@ -51,28 +64,46 @@ router.get('/',function * (){
     this.body = yield render("index");
 });
 router.get('/admin',function * (){
-    yield showUserInfo;
-    this.body = yield render("admin", {
-        // for html's var
-        "userId": userId, 
-        "userName": userName,  
-        "userAccount":userAccount,
-        "time":time,
-        "num": tableNum,
-    });
+    //console.log('>>>>>>>>>>>>>>>>>>'+this.session.user);
+    if (this.session.user ) {
+        console.log('admin')
+        yield showUserInfo;
+        this.body = yield render("admin", {
+            // for html's var
+            "userId": userId, 
+            "userName": userName,  
+            "userAccount":userAccount,
+            "time":time,
+            "num": tableNum,
+        });
+     }
+    else {
+        this.redirect("/");
+     }    
 });
 
 router.post('/',function * (){
     this.body = yield render("index");
     login = this.request.body;
-    user = login['adminLogin'];
+    username = login['adminLogin'];
     psw = login['adminPassword'];
-    console.log('user:'+user+", password:"+ psw);
-    if(user =="admin" && psw =="admin"){
-        this.redirect('/admin');
-    }else{
-        console.log('password error!');
-    }
+    if (username=='Admin'&&psw==='password') {
+        //保存登入狀態
+       console.log(this.session);
+       this.session.user = username;
+       this.redirect("admin");
+   }
+   else{
+       this.redirect("/");
+   }
+
+
+    // console.log('user:'+user+", password:"+ psw);
+    // if(user =="admin" && psw =="admin"){
+    //     this.redirect('/admin');
+    // }else{
+    //     console.log('password error!');
+    // }
 });
 
 app.use(router.middleware());
